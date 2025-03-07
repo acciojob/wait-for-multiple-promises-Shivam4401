104f1fe2def8350b7a0eb00cc852e5b44b7e87cf
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-07 22:10:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
-function createPromise () {
+document.addEventListener('DOMContentLoaded', function() {
+	function createPromise () {
 	return new Promise ((resolve,reject) => {
        let time = (Math.random() * (3000-1000) +1000).toFixed(3);
 		setTimeout(() => {
@@ -14,9 +15,7 @@ let promise3 = createPromise();
 
 Promise.all([promise1,promise2,promise3]).then((data) => {
 	console.log(data);
-})
-
-document.getElementById("output").innerHTML = `
+	document.getElementById("output").innerHTML = `
     <tr> 
 	  <td>Promise 1 </td>
 	  <td>${data[0]} </td>
@@ -34,6 +33,11 @@ document.getElementById("output").innerHTML = `
 	  <td>${Math.max(...data)} </td>
 	</tr>
 `;
+})
+
+})
+
+
 
 
 
